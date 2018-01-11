ff8a5488797d4fb36e92d481a3ef8dc47abfb905
krzysiek nowakowski <n.krzysiek@gmail.com>
2018-01-11 22:07:10 +0100

    or, and implemented in Predicate

--- src/main/java/dataProcessingWithLambdas/Main.java
+++ src/main/java/dataProcessingWithLambdas/Main.java
@@ -2,9 +2,15 @@ package dataProcessingWithLambdas;
 
 public class Main {
     public static void main(String[] args) {
-        Predicate<String> p = s -> s.length() < 20;
+        Predicate<String> p1 = s -> s.length() < 20;
+        Predicate<String> p2 = s -> s.length() > 5;
 
-        boolean b = p.test("aa");
+        boolean b = p1.test("aa");
         System.out.println(b);
+
+        Predicate<String> p3 = p1.and(p2);
+        Predicate<String> p4 = p1.or(p2);
+
+
     }
 }
--- src/main/java/dataProcessingWithLambdas/Predicate.java
+++ src/main/java/dataProcessingWithLambdas/Predicate.java
@@ -3,4 +3,12 @@ package dataProcessingWithLambdas;
 @FunctionalInterface
 public interface Predicate<T> {
     boolean test(T t);
+
+    default Predicate<T> and(Predicate<T> other) {
+        return t -> this.test(t) && other.test(t);
+    }
+
+    default Predicate<T> or(Predicate<T> other) {
+        return t -> this.test(t) || other.test(t);
+    }
 }
