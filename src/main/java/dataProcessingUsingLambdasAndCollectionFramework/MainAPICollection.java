24059e84fb3a1ecfd1dd6be2a9958553f8164da7
krzysiek nowakowski <n.krzysiek@gmail.com>
2018-01-12 19:00:35 +0100

    List.sort(), replaceAll()

--- src/main/java/dataProcessingUsingLambdasAndCollectionFramework/MainAPICollection.java
+++ src/main/java/dataProcessingUsingLambdasAndCollectionFramework/MainAPICollection.java
@@ -3,6 +3,7 @@ package dataProcessingUsingLambdasAndCollectionFramework;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 
 public class MainAPICollection {
@@ -11,16 +12,23 @@ public class MainAPICollection {
         Person p2 = new Person("Brian", 56);
         Person p3 = new Person("Chelsea", 46);
         Person p4 = new Person("David", 28);
-        Person p5 = new Person("Erica", 37);
+        Person p5 = new Person("Erica", 28);
         Person p6 = new Person("Francisco", 18);
-
         List<Person> people = new ArrayList<>(Arrays.asList(p1, p2, p3, p4, p5, p6));
-
 
 //        people.forEach(p -> System.out.println(p));
 //        people.forEach(System.out::println);
 
-        people.removeIf(p -> p.getAge() < 30);
+        // remove.If()
+//        people.removeIf(p -> p.getAge() < 30);
+//        people.forEach(System.out::println);
+
+        //replaceAll()
+//        people.replaceAll(person -> new Person(person.getName().toUpperCase(), person.getAge()));
+//        people.forEach(System.out::println);
+
+        // sort()
+        people.sort(Comparator.comparing(Person::getAge).thenComparing(Person::getName));
         people.forEach(System.out::println);
     }
 }
