17f2cc4fddf595b2550c101477335b48e1695dc1
krzysiek nowakowski <n.krzysiek@gmail.com>
2018-01-12 18:24:54 +0100

    Map.compute*()

--- src/main/java/dataProcessingUsingLambdasAndCollectionFramework/notes/Maps.java
+++ src/main/java/dataProcessingUsingLambdasAndCollectionFramework/notes/Maps.java
@@ -14,21 +14,43 @@ public class Maps {
         City poznan = new City("Poznan");
         List<Person> citizenP = new ArrayList<>(Arrays.asList(alice));
 
-
-
         // Map.forEach()
-        map.forEach((city, list) -> System.out.println(city.getName() + ": " + list.size() + " people"));
+//        map.forEach((city, list) -> System.out.println(city.getName() + ": " + list.size() + " people"));
 
         // Map.getOrDefault()
-        List<Person> defaultList = Arrays.asList();
-        System.out.println(map.getOrDefault(poznan, defaultList));
-        System.out.println(map.getOrDefault(krakow, Collections.emptyList()));
+//        List<Person> defaultList = Arrays.asList();
+//        System.out.println(map.getOrDefault(poznan, defaultList));
+//        System.out.println(map.getOrDefault(krakow, Collections.emptyList()));
 
         // Map.putIfAbsent()
-        System.out.println("Map.putIfAbsent():");
-        map.putIfAbsent(poznan, new ArrayList<>());
-        map.get(poznan).add(alice);
+//        System.out.println("Map.putIfAbsent():");
+//        map.putIfAbsent(poznan, new ArrayList<>());
+//        map.get(poznan).add(alice);
+//        System.out.println(map);
+
+        // Map.replace()
+//        map.replace(krakow, citizenP);
+//        map.replace(krakow, citizenK, citizenP);
+//        map.replace(krakow, citizenP, citizenK);
+//        System.out.println(map);
+
+
+        // Map.replaceAll()
+//        map.replaceAll((city, citizens) -> citizenK);
+//        System.out.println(map);
+
+        // Map.computeIfAbsent()
+//        map.computeIfAbsent(poznan, p -> citizenP);
+//        map.computeIfPresent(
+//                poznan,
+//                (city, people) -> city.getName().equals("Krakow")? citizenK : Collections.emptyList()
+//                );
+        map.compute(
+                krakow,
+                (city, people) -> people
+        );
         System.out.println(map);
+
     }
 
 }
