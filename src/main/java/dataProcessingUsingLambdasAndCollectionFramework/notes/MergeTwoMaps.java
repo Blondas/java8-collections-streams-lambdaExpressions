6b328c2c1ebd2dd4647e198f9b53acc7ed788f74
krzysiek nowakowski <n.krzysiek@gmail.com>
2018-01-12 18:54:53 +0100

    merge two maps

--- src/main/java/dataProcessingUsingLambdasAndCollectionFramework/notes/MergeTwoMaps.java
+++ src/main/java/dataProcessingUsingLambdasAndCollectionFramework/notes/MergeTwoMaps.java
@@ -1,13 +1,13 @@
-package dataProcessingUsingLambdasAndCollectionFramework;
+package dataProcessingUsingLambdasAndCollectionFramework.notes;
 
 import com.krisdrum.lambdaExpressions.commonHelperClasses.City;
 import com.krisdrum.lambdaExpressions.commonHelperClasses.Person;
+
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class Merge {
-
+public class MergeTwoMaps {
     public static void main(String[] args) {
         Map<City, List<Person>> map1 = new HashMap<>();
         Map<City, List<Person>> map2 = new HashMap<>();
