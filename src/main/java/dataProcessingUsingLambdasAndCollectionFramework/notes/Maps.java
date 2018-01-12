5bc71de6036502f54280ef13eac5b8e8f696ca80
krzysiek nowakowski <n.krzysiek@gmail.com>
2018-01-12 15:06:30 +0100

    Map.putIfAbsnet()

--- src/main/java/dataProcessingUsingLambdasAndCollectionFramework/notes/Maps.java
+++ src/main/java/dataProcessingUsingLambdasAndCollectionFramework/notes/Maps.java
@@ -2,9 +2,7 @@ package dataProcessingUsingLambdasAndCollectionFramework.notes;
 
 import com.krisdrum.lambdaExpressions.commonHelperClasses.City;
 import dataProcessingUsingLambdasAndCollectionFramework.Person;
-
 import java.util.*;
-
 import static dataProcessingUsingLambdasAndCollectionFramework.Fixtures.*;
 
 public class Maps {
@@ -13,18 +11,24 @@ public class Maps {
         map.put(krakow, citizenK);
         map.put(warszawa, citizenW);
 
+        City poznan = new City("Poznan");
+        List<Person> citizenP = new ArrayList<>(Arrays.asList(alice));
+
+
+
         // Map.forEach()
-        map.forEach((city, list) -> System.out.println(
-                city.getName() + ": " + list.size() + " people"
-        ));
+        map.forEach((city, list) -> System.out.println(city.getName() + ": " + list.size() + " people"));
 
-        // Map.getOrDefault
+        // Map.getOrDefault()
         List<Person> defaultList = Arrays.asList();
-        System.out.println(
-                map.getOrDefault(new City("Poznan"), defaultList)
-        );
-        System.out.println(map.getOrDefault(krakow, defaultList));
+        System.out.println(map.getOrDefault(poznan, defaultList));
+        System.out.println(map.getOrDefault(krakow, Collections.emptyList()));
 
+        // Map.putIfAbsent()
+        System.out.println("Map.putIfAbsent():");
+        map.putIfAbsent(poznan, new ArrayList<>());
+        map.get(poznan).add(alice);
+        System.out.println(map);
     }
 
 }
